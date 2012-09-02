7a224c3a09c2b51227490de09e1fac4060911d50
Ian Halpern <ian.halpern@rentshare.com>
2012-09-02 00:53:50 -0400

    * added a dummy gateway

---
+++ payment_processor/gateways/dummy.py
@@ -0,0 +1,9 @@
+from payment_processor.exceptions import *
+from payment_processor.gateway import BaseGateway
+import requests
+
+class Dummy(BaseGateway):
+	def _authorize( self, transaction ):
+		pass
+	def _charge( self, transaction ):
+		pass

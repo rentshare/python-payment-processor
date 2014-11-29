6406d7193dd459fa27c06302fa76f6fafa374c1c
Ian Halpern <ian.halpern@rentshare.com>
2014-11-29 17:06:17 -0500

    Updated dummy gateway to return transaction ids

--- payment_processor/gateways/dummy.py
+++ payment_processor/gateways/dummy.py
@@ -1,30 +1,34 @@
 from payment_processor.exceptions import *
 from payment_processor.gateway import BaseGateway
+import random
 
 class Dummy(BaseGateway):
     """Dummy gateway."""
     provider = 'dummy'
 
+    def _rand_id(self):
+        return str(int(random.random()*1000000))
+
     def _charge(self, transaction):
         """Authorize and capture the transaction."""
-        pass
+        return self._rand_id()
 
     def _authorize(self, transaction):
         """Authorize the transaction."""
-        pass
+        return self._rand_id()
 
     def _capture(self, transaction):
         """Capture the transaction."""
-        pass
+        return transaction.transaction_id
 
     def _refund(self, transaction):
         """Refund the transaction."""
-        pass
+        return self._rand_id()
 
     def _credit(self, transaction):
         """Credit the transaction."""
-        pass
+        return self._rand_id()
 
     def _void(self, transaction):
         """Void the transaction."""
-        pass
+        return transaction.transaction_id
